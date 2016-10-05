1c366e0a2d6b4c8284d089c8e9336c9cab2bb851
James Hu <hello@james.hu>
2016-10-05 13:10:34 -0700

    Add version method

--- lib/reverse_proxy.rb
+++ lib/reverse_proxy.rb
@@ -2,4 +2,9 @@ require 'reverse_proxy/client'
 require 'reverse_proxy/controller'
 
 module ReverseProxy
+  def version
+    File.open(File.expand_path("../../VERSION", __FILE__)).read.strip
+  end
+
+  extend self
 end
